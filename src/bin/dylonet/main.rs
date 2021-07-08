5ef85330b2e8c9ef2332b20ad530c750ee8bb7b3
Florian Eich <florian@bmc-labs.com>
2021-07-08 13:36:02 +0200

    Add dylonet main

---
+++ src/bin/dylonet/main.rs
@@ -0,0 +1,11 @@
+// Copyright 2021 bmc::labs GmbH.
+//
+// Authors:
+//   Florian Eich <florian@bmc-labs.com>
+//
+// This work is licensed under the Apache License, Version 2.0. You should have
+// received a copy of this license along with the source code. If that is not
+// the case, please find one at http://www.apache.org/licenses/LICENSE-2.0.
+
+
+
